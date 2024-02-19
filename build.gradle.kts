879e710d6658061890c4f9331710db92b329c06d
BreninSul <42633769+BreninSul@users.noreply.github.com>
2024-02-19 23:07:12 +0200

    Organisation clients

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,7 +32,7 @@ buildscript {
 }
 
 group = "com.alcosi.nft"
-version = "12.06"
+version = "12.07"
 java.sourceCompatibility = JavaVersion.VERSION_21
 val web3jVersion = "4.10.3"
 val jjwtVersion = "0.12.3"
