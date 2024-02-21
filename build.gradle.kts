e63e10a0aa597d07291937827e3f76eaafed98ec
BreninSul <42633769+BreninSul@users.noreply.github.com>
2024-02-21 17:49:49 +0200

    Added possibility to configure authorisation groups check mode

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,7 +32,7 @@ buildscript {
 }
 
 group = "com.alcosi.nft"
-version = "12.12"
+version = "12.13"
 java.sourceCompatibility = JavaVersion.VERSION_21
 val web3jVersion = "4.10.3"
 val jjwtVersion = "0.12.3"
