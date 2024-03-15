cb90fbc125eb468014138b4a678f9f5257069c12
BreninSul <42633769+BreninSul@users.noreply.github.com>
2024-03-15 14:22:06 +0200

    Remove copyright notices and update version in build.gradle.kts
    Removed the copyright comments at the start of various .sql files in the migration directory, ensuring a cleaner, focused codebase. The version number in the build.gradle.kts file was also updated from "13.0" to "13.3" reflecting new changes in the project.

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,7 +32,7 @@ buildscript {
 }
 
 group = "com.alcosi.nft"
-version = "13.0"
+version = "13.3"
 java.sourceCompatibility = JavaVersion.VERSION_21
 val web3jVersion = "4.10.3"
 val jjwtVersion = "0.12.3"
