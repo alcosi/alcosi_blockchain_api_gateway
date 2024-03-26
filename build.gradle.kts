069f08897903a6fe8d8e8ea456d91d2679756d46
BreninSul <42633769+BreninSul@users.noreply.github.com>
2024-03-26 21:18:59 +0100

    Update application version and encrypting keys
    Updated the version in build.gradle.kts from "14.0" to "14.1". Also commented out the private and public encryption keys in application.properties. This was done for security reasons and the keys will now be passed through a secure method.

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,7 +32,7 @@ buildscript {
 }
 
 group = "com.alcosi.nft"
-version = "14.0"
+version = "14.1"
 java.sourceCompatibility = JavaVersion.VERSION_21
 val web3jVersion = "4.10.3"
 val jjwtVersion = "0.12.3"
